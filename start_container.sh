f743d28e47a9787eb29ac0242398e9b2e65a584c
pthiel <philipp.thiel@cgc.mpg.de>
2019-03-18 10:09:13 +0100

    Improved startup script

--- start_container.sh
+++ start_container.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 
 #----------------------------------
+# Important Settings
+#----------------------------------
+
+if [ -z "$ABI_SERVICES_CONTACT_MAIL" ]
+then
+  contact_email="abi-services@informatik.uni-tuebingen.de"
+else
+  contact_email="$ABI_SERVICES_CONTACT_MAIL"
+fi
+
+if [ -z "$ABI_SERVICES_IMPRINT_URL" ]
+then
+  imprint_url="https://www-abi.informatik.uni-tuebingen.de/imprint"
+else
+  imprint_url="$ABI_SERVICES_IMPRINT_URL"
+fi
+
+if [ -z "$ABI_SERVICES_GDPR_URL" ]
+then
+  gdpr_url="https://www-abi.informatik.uni-tuebingen.de/gdpr"
+else
+  gdpr_url="$ABI_SERVICES_GDPR_URL"
+fi
+
+if [ -z "$ABI_SERVICES_YLOC_PORT" ]
+then
+  yloc_port="28010"
+else
+  yloc_port="$ABI_SERVICES_YLOC_PORT"
+fi
+
+
+#----------------------------------
 # Start YLoc Daemon Container
 #----------------------------------
 
-contact_email="abi-services@informatik.uni-tuebingen.de"
-imprint_url="https://www-abi.informatik.uni-tuebingen.de/imprint"
-gdpr_url="https://www-abi.informatik.uni-tuebingen.de/gdpr"
-
-docker run --rm -it -d -p 28010:80 \
+docker run --rm -it -d -p $yloc_port:80 \
            -e YL_CONTACT_EMAIL="$contact_email" \
            -e YL_IMPRINT_URL="$imprint_url" \
            -e YL_GDPR_URL="$gdpr_url" \
